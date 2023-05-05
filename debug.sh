cf98b4ed6c1337f762fc46b844cbf0d27802ff7e
HitsujiRere <hitsujirere@gmail.com>
2023-05-05 13:10:38 +0900

    make-abcを追加

--- debug.sh
+++ debug.sh
@@ -1,2 +1,3 @@
 g++ -std=gnu++17 -O2 -Wall -Wextra -o $1 -Ilib/ -Ilib/ac-library $1.cpp -DDEBUG_BUILD
+echo "Compiled!"
 ./$1
---
+++ make-abc.sh
@@ -0,0 +1,7 @@
+mkdir src/$1
+cp template/main.cpp src/$1/a.cpp
+cp template/main.cpp src/$1/b.cpp
+cp template/main.cpp src/$1/c.cpp
+cp template/main.cpp src/$1/d.cpp
+cp template/main.cpp src/$1/e.cpp
+cp template/main.cpp src/$1/f.cpp
