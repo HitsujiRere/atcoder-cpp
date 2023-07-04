f66c545a0d8dd10cbdb103edf9f0889479891aa8
root <root@PC-04.localdomain>
2023-07-04 10:50:45 +0900

    Add compiling message to run/debug

--- debug.sh
+++ debug.sh
@@ -1,3 +1,4 @@
+echo "Compiling..."
 g++ -std=gnu++17 -O2 -Wall -Wextra -o $1 -Ilib/ -Ilib/ac-library $1.cpp -DDEBUG_BUILD
 echo "Compiled!"
 ./$1
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
+echo "Compiling..."
 g++ -std=gnu++17 -O2 -Wall -Wextra -o $1 -Ilib/ -Ilib/ac-library $1.cpp
+echo "Compiled!"
 ./$1
