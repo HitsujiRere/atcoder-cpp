1caa7e8008b2c896648b450dc15b537080382a01
root <root@PC-04.localdomain>
2023-07-12 11:21:12 +0900

    abc一括作成時に開くように

--- make-abc.sh
+++ make-abc.sh
@@ -1,7 +1,13 @@
 mkdir src/$1
 cp template/main.cpp src/$1/a.cpp
+code src/$1/a.cpp
 cp template/main.cpp src/$1/b.cpp
+code src/$1/b.cpp
 cp template/main.cpp src/$1/c.cpp
+code src/$1/c.cpp
 cp template/main.cpp src/$1/d.cpp
+code src/$1/d.cpp
 cp template/main.cpp src/$1/e.cpp
+code src/$1/e.cpp
 cp template/main.cpp src/$1/f.cpp
+code src/$1/f.cpp
