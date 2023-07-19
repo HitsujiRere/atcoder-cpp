d2592a8dea97bc4d01de9c3156b6f245594017a3
root <root@PC-04.localdomain>
2023-07-19 10:00:44 +0900

    Rename exefile to *_dbg.exe

--- debug.sh
+++ debug.sh
@@ -1,13 +1,13 @@
-if [ -e $1 ]; then
-    timeExe=`date +%Y%m%d%H%M%S -r $1`
+if [ -e $1_dbg ]; then
+    timeExe=`date +%Y%m%d%H%M%S -r $1_dbg`
 else
     timeExe="0"
 fi
 timeCpp=`date +%Y%m%d%H%M%S -r $1.cpp`
 if [ $timeCpp -ge $timeExe ]; then
     echo "Compiling..." >&2
-    g++ -std=gnu++17 -O2 -Wall -Wextra -o $1 -Ilib/ -Ilib/ac-library $1.cpp -DDEBUG_BUILD
+    g++ -std=gnu++17 -O2 -Wall -Wextra -o $1_dbg -Ilib/ -Ilib/ac-library $1.cpp -DDEBUG_BUILD
     echo "Compiled!" >&2
 fi
 echo "Run with debug-mode:" >&2
-./$1
+./$1_dbg
