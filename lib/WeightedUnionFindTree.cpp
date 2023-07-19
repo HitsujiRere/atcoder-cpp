e2b19199da85064378675c0c86ddcf31666f7614
root <root@PC-04.localdomain>
2023-07-19 09:59:51 +0900

    Add WeightedUnionFindTree to lib

--- lib/WeightedUnionFindTree.cpp
+++ lib/WeightedUnionFindTree.cpp
@@ -2,41 +2,57 @@
 using namespace std;
 
 // Union Find Tree
+template <class T>
 struct UnionFindTree {
     // 集合のサイズ
     vector<int> sizes;
     // xの親
-    vector<int> parents;
+    vector<int> parent;
+    // 重み
+    vector<T> diffweights;
 
     // n個の木を作成
     UnionFindTree(int n) {
         assert(n >= 0);
         sizes.resize(n);
-        parents.resize(n);
+        parent.resize(n);
+        diffweights.resize(n);
         for (int i = 0; i < n; i++) makeTree(i);
     }
 
     // xを含む木の根を求める
     int findRoot(int x) {
         assert(x >= 0);
-        if (x != parents[x]) parents[x] = findRoot(parents[x]);
-        return parents[x];
+        compress();
+        return parent[x];
     }
 
     // xを含む集合の要素数を求める
     int treeSize(int x) { return sizes[findRoot(x)]; }
 
+    // 重さを求める
+    int weight(int x) {
+        compress();
+        return diffweights[x];
+    }
+
     // xとyが同じ集合にいるか。計算量はO(α(n))
     bool isSame(int x, int y) { return findRoot(x) == findRoot(y); }
 
     // xとyを同じ集合にする。計算量はO(α(n))
-    bool unite(int x, int y) {
+    bool unite(int x, int y, T w) {
+        w += weight(x);
+        w -= weight(y);
         x = findRoot(x);
         y = findRoot(y);
         if (x == y) return false;
-        if (sizes[x] < sizes[y]) swap(x, y);
-        parents[y] = x;
+        if (sizes[x] < sizes[y]) {
+            swap(x, y);
+            w *= -1;
+        }
+        parent[y] = x;
         sizes[x] += sizes[y];
+        diffweights[y] += w;
         return true;
     }
 
@@ -44,7 +60,18 @@ struct UnionFindTree {
     // xを根とする木を作成する
     void makeTree(int x) {
         assert(x >= 0);
-        parents[x] = x;
+        parent[x] = x;
         sizes[x] = 1;
+        diffweights[x] = 0;
+    }
+
+    // 圧縮する
+    void compress(int x) {
+        assert(x >= 0);
+        if (x == parent[x]) return;
+        if (parent[x] == parent[parent[x]]) return;
+        const int root = findRoot(parent[x]);
+        diffweights[x] += diffweights[parent[x]];
+        parent[x] = root;
     }
 };
