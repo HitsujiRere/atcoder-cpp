a316320b64bd1165054e15759034d3cfc1b00db1
HitsujiRere <hitsujirere@gmail.com>
2023-10-02 20:06:26 +0900

    Rename executable files

--- debug.sh
+++ debug.sh
@@ -1,13 +1,13 @@
-if [ -e $1_dbg ]; then
-    timeExe=`date +%Y%m%d%H%M%S -r $1_dbg`
+if [ -e $1_dev ]; then
+    timeExe=`date +%Y%m%d%H%M%S -r $1_dev`
 else
     timeExe="0"
 fi
 timeCpp=`date +%Y%m%d%H%M%S -r $1.cpp`
 if [ $timeCpp -ge $timeExe ]; then
     echo "Compiling..." >&2
-    g++ -std=gnu++17 -O2 -Wall -Wextra -o $1_dbg -Ilib/ -Ilib/ac-library $1.cpp -DDEBUG_BUILD
+    g++ -std=gnu++17 -O2 -Wall -Wextra -o $1_dev -Ilib/ -Ilib/ac-library $1.cpp -DDEBUG_BUILD
     echo "Compiled!" >&2
 fi
-echo "Run with debug-mode:" >&2
-./$1_dbg
+echo "Run (develop mode):" >&2
+./$1_dev
--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
-if [ -e $1 ]; then
-    timeExe=`date +%Y%m%d%H%M%S -r $1`
+if [ -e $1_pro ]; then
+    timeExe=`date +%Y%m%d%H%M%S -r $1_pro`
 else
     timeExe="0"
 fi
 timeCpp=`date +%Y%m%d%H%M%S -r $1.cpp`
 if [ $timeCpp -ge $timeExe ]; then
     echo "Compiling..." >&2
-    g++ -std=gnu++17 -O2 -Wall -Wextra -o $1 -Ilib/ -Ilib/ac-library $1.cpp
+    g++ -std=gnu++17 -O2 -Wall -Wextra -o $1_pro -Ilib/ -Ilib/ac-library $1.cpp -DDEBUG_BUILD
     echo "Compiled!" >&2
 fi
-echo "Run:" >&2
-./$1
+echo "Run (develop mode):" >&2
+./$1_pro
