9ac6303f6a928e80519384f9b50c6868cc962e65
HitsujiRere <hitsujirere@gmail.com>
2023-10-02 20:19:59 +0900

    Add time.cpp

---
+++ template/time.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    auto start = chrono::system_clock::now();
+    auto end = chrono::system_clock::now();
+    auto msec =
+        chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+    return 0;
+}
