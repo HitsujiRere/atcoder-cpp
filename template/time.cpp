5c5a939da119d0cb57739f4442f21ebc0272b231
HitsujiRere <hitsujirere@gmail.com>
2023-10-02 21:00:14 +0900

    Fix template/time.cpp

--- template/time.cpp
+++ template/time.cpp
@@ -3,8 +3,9 @@ using namespace std;
 using ll = long long;
 
 int main() {
-    chrono::time_point start = chrono::system_clock::now();
-    chrono::time_point end = chrono::system_clock::now();
+    chrono::time_point<chrono::system_clock> start =
+        chrono::system_clock::now();
+    auto end = chrono::system_clock::now();
     ll msec = chrono::duration_cast<chrono::milliseconds>(end - start).count();
 
     return 0;
