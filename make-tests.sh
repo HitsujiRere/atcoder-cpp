8ff9228a97b78386066b60e46473cb193a80a1f3
HitsujiRere <hitsujirere@gmail.com>
2023-10-02 23:13:17 +0900

    Fix executable files

--- make-tests.sh
+++ make-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 dirs=`find $2/in/* -maxdepth 0 -type f`
-g++ -std=gnu++17 -O2 -Wall -Wextra -o $1 -Ilib/ -Ilib/ac-library $1.cpp
+g++ -std=gnu++17 -O2 -Wall -Wextra -o $1_pro -Ilib/ -Ilib/ac-library $1.cpp
 mkdir $2/out/
 for dir in $dirs;
 do
     echo $dir
-    ./$1 < $dir > $2/out/${dir##*/}
+    ./$1_pro < $dir > $2/out/${dir##*/}
 done
--- mark.sh
+++ mark.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 dirs=`find $2/in/* -maxdepth 0 -type f`
-g++ -std=gnu++17 -O2 -Wall -Wextra -o $1 -Ilib/ -Ilib/ac-library $1.cpp
+g++ -std=gnu++17 -O2 -Wall -Wextra -o $1_pro -Ilib/ -Ilib/ac-library $1.cpp
 sum=0
 for dir in $dirs;
 do
-    score=`cat $2/in/${dir##*/} $2/out/${dir##*/} | ./$1`
+    score=`cat $2/in/${dir##*/} $2/out/${dir##*/} | ./$1_pro`
     echo $dir: $score
     sumscore=$(($sumscore+$score))
 done
