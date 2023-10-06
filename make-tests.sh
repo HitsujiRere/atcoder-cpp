c25a1def71085b34a12f2fa449c131c90d1d2705
HitsujiRere <hitsujirere@gmail.com>
2023-10-06 10:28:39 +0900

    Fix make-tests.sh

--- make-tests.sh
+++ make-tests.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 dirs=`find $2/in/* -maxdepth 0 -type f`
 g++ -std=gnu++17 -O2 -Wall -Wextra -o $1_pro -Ilib/ -Ilib/ac-library $1.cpp
-mkdir $2/out/
+mkdir -p $2/out/
 for dir in $dirs;
 do
-    echo $dir
+    echo "-----"
+    echo $dir "->" $2/out/${dir##*/}
     ./$1_pro < $dir > $2/out/${dir##*/}
 done
