f809ed68a50ec1432439f98e728d157c525f27b0
HitsujiRere <hitsujirere@gmail.com>
2023-10-10 07:56:04 +0900

    Fix memo/random.cpp

--- memo/random.cpp
+++ memo/random.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     random_device seed_gen;
-    mt19937_64 engine(seed_gen);
+    mt19937_64 engine(seed_gen());
 
     // 1以上100以下の離散一様分布
     uniform_int_distribution<> rand(1, 100);
